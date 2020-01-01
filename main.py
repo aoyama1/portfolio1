a598d83511d69dda7c080cebfd87f2fbe3ec0b87
aoyama <aoyama>
2020-01-01 17:03:48 +0900

    login_app

--- main.py
+++ main.py
@@ -1,21 +1,84 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
+app.secret_key = b'random string...'
+
+member_data = {}
+message_data = []
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html',\
-        title="ROPE'S sample",\
-            message="お名前は？")
+    global message_data
+    if 'login' in session and session['login']:
+        msg ='Login id:' + session['id']
+        return render_template('messages.html',\
+        title="Messages",\
+        message=msg,\
+        data=message_data )
+    else:
+        return redirect('/login')
 
 @app.route('/', methods=['POST'])
 def form():
-    field =request.form['field']
-    return render_template('index.html', \
-        title="ROPE'S sample",\
-            message="こんちには、%sさん!" % field)
+    msg =request.form.get('comment')
+    message_data.append((session['id'], msg))
+    if len(message_data) >25:
+        message_data.pop(0)
+    return redirect('/')
 
+# login page access
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html', \
+    title='Login', \
+        err=False, \
+        message='IDとパスワードを入力:', \
+            id='' )
+
+#login form sended.
+@app.route('/login', methods=['POST'])
+def login_post():
+    global member_data
+    id = request.form.get('id')
+    pswd = request.form.get('pass')
+    if id in member_data:
+        if pswd ==member_data[id]:
+            session['login'] =True
+        else:
+            session['login'] = False
+    else:
+        member_data[id] =pswd
+        session['login'] = True
+    session['id'] = id
+    if session['login']:
+        return redirect('/')
+    else:
+        return render_template('login.html',\
+            title='Login',\
+            err=False, \
+            message="パスワードが違います",\
+            id=id)
+# logout
+@app.route('/logout' ,methods=['GET'])
+def logout():
+    session.pop('id', None)
+    session.pop('login') 
+    return redirect('/login')           
+        
+
+
+#    field =request.form['field']
+#    return render_template('index.html', \
+#        title="ROPE'S sample",\
+#            message="こんちには、%sさん!" % field)
 
 if __name__ == '__main__':
     app.run()
+
+#@app.route('/next', methods=['GET'])
+#def next():
+#    return render_template('next.html', \
+#    title='Next page', \
+#    message='※これは、別ページ', \
+#    data= ['One', 'Two', 'Three'])
 
